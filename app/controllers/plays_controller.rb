82564ced6d5f478fe3b2c1e2645dc1e52de429a7
BLaurenB <blbillington1@gmail.com>
2018-02-21 10:08:58 -0700

    switched to forg_tag

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,13 +3,13 @@ class PlaysController < ApplicationController
   def create
 
     if params[:word].is_valid?
-        Play.create(user)game_id, user_id, word
+        Play.create(game_id: 1, user_id: 1, word: params[:word])
         # maybe if user_id is eve, it's player_2?
-
     redirect_to root_path
     else
       flash[:notice] = "Oh no! That wasn't a word!"
       redirect to root_path
     end
+  end 
 
 end
