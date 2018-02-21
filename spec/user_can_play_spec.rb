d76dd8f58875f03f2da93f11da21e91623dbf7c8
BLaurenB <blbillington1@gmail.com>
2018-02-21 09:37:39 -0700

    about to start faraday request and validation

--- spec/user_can_play_spec.rb
+++ spec/user_can_play_spec.rb
@@ -9,10 +9,12 @@ describe "As a user" do
     # As a user
     # When I visit "/"
     # And I fill in a text box with "foxes"
-    fill_in "word", with: "foxes"
+    fill_in "play_word", with: "foxes"
     # And I click "Validate Word"
     click_button "Validate Word"
     # Then I should see a message that says "'foxes' is a valid word and its root form is 'fox'."
+# This needs to post to the play#create method
+
     expect(page).to have_content("'foxes' is a valid word and its root form is 'fox'")
   end
 
